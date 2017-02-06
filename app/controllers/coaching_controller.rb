4d5eaae6d4df881c49e0ea42308d9dc79b3f7902
Justine Courtois <justine.crts@gmail.com>
2017-02-06 12:30:25 +0100

    form

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,16 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+    if @query == "I am going to work right now!"
+      @answer = "Goodbye!"
+    elsif @query.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+
+end
